6d147dc2ea214f1b62d5318a91ed812a11b97d81
Vinay <kondojuvinaykumar2004@gmail.com>
2026-06-22 12:35:53 +0530

    remove unused cognito module

--- modules/cognito/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "user_pool_id" { value = aws_cognito_user_pool.pool.id }
-output "user_pool_arn" { value = aws_cognito_user_pool.pool.arn }
-output "client_id" { value = aws_cognito_user_pool_client.client.id }
